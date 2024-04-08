01ef297d17a2c08c8571f52c943daa59cf2e7542
AustinP1121 <m1215550@moreheadstate.edu>
2024-04-08 09:34:12 -0400

    modified:   EC_CHDS_Data_Handler.c 	new file:   includes/mcp4725_interface.c 	new file:   includes/mcp4725_interface.h 	new file:   includes/nrf24l01_interface.c 	new file:   includes/nrf24l01_interface.h

--- EC_CHDS_Data_Handler.c
+++ EC_CHDS_Data_Handler.c
@@ -1,13 +1,32 @@
-
 /*Std includes*/
 #include <stdio.h>
 #include <stdlib.h>
 #include </usr/include/i2c/smbus.h>
 #include <linux/i2c-dev.h>
 
+
 int main()
 {
+    int file;
+    //using i2c-1
+    int adapter_nr = 1;
+    char filename[20];
+    char buf[3];
+    int mcp4725_addr = 0x62;
+
+    snprintf(filename, 19, "/dev/i2c-%d", adapter_nr);
+    file = open(filename, 0_RDWR);
+    if(file < 0)
+    {
+        exit(1);
+    }
     
+    //select the mcp4725 on the I2C bus
+    if(ioctl(file, I2C_SLAVE, mcp4725_addr) < 0)
+    {
+        printf("\nslave not found\n");
+        exit(1);
+    }
 
     return 0;
 }
