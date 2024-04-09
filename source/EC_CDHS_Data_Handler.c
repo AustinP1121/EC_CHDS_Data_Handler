6206427a8dbffc327d722d4e4d86042f4dd1a481
AustinP1121 <m1215550@moreheadstate.edu>
2024-04-09 19:26:55 -0400

    test

--- source/EC_CDHS_Data_Handler.c
+++ source/EC_CDHS_Data_Handler.c
@@ -1,11 +1,14 @@
 /*Std includes*/
 #include <stdio.h>
 #include <stdlib.h>
-#include </usr/include/i2c/smbus.h>
 #include <linux/i2c-dev.h>
+#include </usr/include/i2c/smbus.h>
+#include <fcntl.h> // Include the necessary header file for 'open' function
+#include <sys/ioctl.h> // Include the necessary header file for 'ioctl' function
 
+//#include "../includes"
 
-int main()
+int main(int argc, char *argv[])
 {
     int file;
     //using i2c-1
@@ -15,12 +18,13 @@ int main()
     int mcp4725_addr = 0x62;
 
     snprintf(filename, 19, "/dev/i2c-%d", adapter_nr);
-    file = open(filename, 0_RDWR);
-    if(file < 0)
+    file = open(filename, I2C_RDWR);
+    if (file < 0)
     {
+        printf("not found");
         exit(1);
     }
-    
+
     //select the mcp4725 on the I2C bus
     if(ioctl(file, I2C_SLAVE, mcp4725_addr) < 0)
     {
